71ab074daf5b24c172e08a93c5228668c89344de
Carolina Mauro <carolina.mauro@brosoft.com.ar>
2022-06-16 13:56:41 -0300

    librearia con tab

--- App.js
+++ App.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BookList from './src/views/Library/BookList'
 import BookDetail from './src/views/Library/BookDetail'
+import AddBook from './src/views/AddBook/AddBook'
 
+
+const Tab = createBottomTabNavigator()
+const LibraryStack = createStackNavigator();
 
 //creo mi pila de navegación
-const Stack = createStackNavigator();
+//const Stack = createStackNavigator();
+
+function LibraryStackScreen(){
+  return(
+    <LibraryStack.Navigator>
+      <LibraryStack.Screen name='BookList' component={BookList} options={{title: 'Inicio'}} />
+      <LibraryStack.Screen name='BookDetail' component={BookDetail} />
+    </LibraryStack.Navigator>
+  )
+}
 
 const App = () => (
   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name='BookList' component={BookList} options={{title: 'Inicio'}} />
-      <Stack.Screen name='BookDetail' component={BookDetail} />
-    </Stack.Navigator>  
+    <Tab.Navigator> 
+      <Tab.Screen name='LibraryTab' component={LibraryStackScreen} />
+      <Tab.Screen name='AddBookTab' component={AddBook} />
+    </Tab.Navigator>
   </NavigationContainer>
   
 )
 
 export default App;
 
+
+ {/* <Stack.Navigator>
+      <Stack.Screen name='BookList' component={BookList} options={{title: 'Inicio'}} />
+      <Stack.Screen name='BookDetail' component={BookDetail} />
+    </Stack.Navigator>   */}
---
+++ src/views/AddBook/AddBook.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native"
+
+export default function AddBook(){
+  
+  return(
+    <View>
+      <Text> Hola soy la vista "Add Book"!</Text>
+    </View>
+  )
+}
